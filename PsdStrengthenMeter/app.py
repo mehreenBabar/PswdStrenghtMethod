14a5f86278abb55ec2e30754930fb8d540cb4d3f
Mehreen-Babar <mehreenbabar465@gmail.com>
2025-05-26 21:38:08 +0500

    Add files via upload

---
+++ PsdStrengthenMeter/app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+def check_password(password):
+    score = 0
+    tips = []
+    
+    if len(password) >= 8:
+        score += 1
+    else: tips.append("🔴Use atleast 8 characters.")
+    
+    if any(c.isupper()for c in password):
+        score +=1
+    else:tips.append("🟠Include upper letter")
+    if any(c.islower()for c in password):
+        score +=1
+    else:tips.append("🟡Include lower letter")
+    if any(c.isdigit()for c in password):
+        score +=1
+    else:tips.append("🟢Add a number (0-9).")
+    if any(c in "!@#$%^&" for c in password):
+        score +=1
+    else:tips.append("🔵Use a special character (!@#$%^&)") 
+    return score, tips
+
+def main():
+    st.title("🔐Password Strength Meter" )
+    password = st.text_input("Enter Password🔑",type="password")
+
+    if password:
+        score, tips = check_password(password)
+
+        if score == 5:
+            st.success("✅ Strong Password! Secure & Safe.")
+        elif score == 3 or score == 4:
+            st.warning("⚠️ Moder!te Password! Improve it.")
+        else:
+            st.error("❌ Weak Password! Follow these steps:")
+        for tip in tips:
+            st.write(tip)
+main()
